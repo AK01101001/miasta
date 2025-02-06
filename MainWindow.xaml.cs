51c2a43e7204cfa9937b8503a1f8d22db81ccd91
AK01101001 <147595111+AK01101001@users.noreply.github.com>
2025-02-06 12:46:41 +0100

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿
+using System;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace kres
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Miasto> miasta { get; set; } = new List<Miasto>();
+        int index = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dodajMiasta();
+        }
+
+        private void dodajMiasta()
+        {
+            miasta.Add(new Miasto("Jinzhou","Huanglong","Jinzhou",1000000,0,new Uri("obraz3.png")));
+        }
+
+        private void polubienie(object sender, RoutedEventArgs e)
+        {
+            miasta.ElementAt(index).LiczbaPolubien++;
+            polubienia.Text=miasta.ElementAt(index).LiczbaPolubien.ToString();
+        }
+
+        private void lewo(object sender, RoutedEventArgs e)
+        {
+            index--;
+            if (index<0)
+            {
+                index = miasta.Count;
+            }
+            
+            wyswietl();
+        }
+
+
+        private void prawo(object sender, RoutedEventArgs e)
+        {
+            index++;
+            if (index > miasta.Count)
+            {
+                index = 0;
+            }
+        }
+
+        private void wyswietl()
+        {
+            nazwa.Text=miasta.ElementAt(index).Nazwa;
+            kontynent.Text=miasta.ElementAt(index).Kontynent;
+            region.Text=miasta.ElementAt(index).Region;
+            mieszkancy.Text=miasta.ElementAt(index).LiczbaMieszkancow.ToString();
+            polubienia.Text=miasta.ElementAt(index).LiczbaPolubien.ToString();
+            zdjecie.Source = new BitmapImage(miasta.ElementAt(index).zdjecie);
+        }
+    }
+}
---
+++ Miasto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kres
+{
+    public class Miasto
+    {
+        public string Nazwa { get; set; }
+        public string Kontynent { get; set; }
+        public string Region { get; set; }
+        public int LiczbaMieszkancow { get; set; }
+        public int LiczbaPolubien { get; set; }
+        public Uri zdjecie { get; set; }
+
+        public Miasto(string nazwa, string kontynent, string region, int liczbaMieszkancow, int liczbaPolubien, Uri zdjecie)
+        {
+            Nazwa = nazwa;
+            Kontynent = kontynent;
+            Region = region;
+            LiczbaMieszkancow = liczbaMieszkancow;
+            LiczbaPolubien = liczbaPolubien;
+            this.zdjecie = zdjecie;
+        }
+    }
+}
